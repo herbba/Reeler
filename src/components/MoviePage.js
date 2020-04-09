84bf16b6df365370c7ab48ce11c0ab4287f6d3f2
Ella Mäkelä <ella@makela.co>
2020-04-09 13:54:21 +0300

    Korjattu toimimaan API:lla

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -13,54 +13,27 @@ const MoviePage = ({ mov }) => {
     genres: ['genre1', 'genre2']
   }; */
 
-  const renderCast = () => {
-    const cast = [
-      { id: 1, name: 'Actor 1', role: 'role 1' },
-      { id: 2, name: 'Actor 2', role: 'role 3' },
-      { id: 3, name: 'Actor 3', role: 'role 3' }
-    ];
-
-    return cast.map(castMember => (
-      <p key={castMember.id}>
-        {castMember.name} - {castMember.role}
-      </p>
-    ));
+  const runTimeToHours = () => {
+    const runtime = mov.runtimeminutes;
+    const hours = runtime / 60;
+    const rhours = Math.floor(hours);
+    const minutes = (hours - rhours) * 60;
+    const rminutes = Math.round(minutes);
+    return rhours + ' h ' + rminutes + ' min';
   };
 
   return (
-    <div>
-      <div className='movieHeader'>
-        <h2>{mov.primaryTitle}</h2>
-        {mov.endYear ? (
-          <h3>
-            {mov.startYear} - {mov.endYear}
-          </h3>
-        ) : (
-          <h3>{mov.startYear}</h3>
-        )}
-        <div className='movieInfo'>
-          {mov.runTimeMinutes} min | {mov.genres} | julkaisupäivä ja maa
-        </div>
-      </div>
-      <div className='movieImages'>
-        <div>image 1</div>
-        <div>image 2</div>
-        <div>image 3</div>
-      </div>
-      <div className='movieAbstract'>
-        <p>
-          This movie is a movie. And it is a movie and it is very funny lol.
-        </p>
-        <p>director: </p>
-        <p>writers:</p>
-        <p>stars:</p>
-      </div>
-      <div className='movieCast'>
-        <h3>Cast</h3>
-        {renderCast()}
-      </div>
-      <div className='movieStoryline'>
-        <h3>Storyline</h3> This is the storyline
+    <div className='movieHeader'>
+      <h2>{mov.primarytitle}</h2>
+      {mov.endyear ? (
+        <h3>
+          {mov.startyear} - {mov.endyear}
+        </h3>
+      ) : (
+        <h3>{mov.startyear}</h3>
+      )}
+      <div className='movieInfo'>
+        {runTimeToHours()} | {mov.genres.join(', ')}
       </div>
     </div>
   );
