ef81985cec6dd7ce1b5b3e4dafdd6f6e961ee73b
tonimoil <62765827+tonimoil@users.noreply.github.com>
2020-04-16 13:56:38 +0300

    Add files via upload

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,54 +1,63 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react';
+import React, { useState } from 'react';
 import '../Search.css';
 
 const SearchResult = ({ results, onItemClick }) => {
-  /*
-    MOVIE {
-    id: string,
-    titleType: string,
-    primaryTitle: string,
-    originalTitle: string,
-    isAdult: boolean,
-    startYear: number,
-    endYear: number | null,
-    runtimeMinutes: number,
-    genres: [string]
-    }
+  const [mVisible, setMVisible] = useState(4);
+  const [aVisible, setAVisible] = useState(4);
+  const [movies, setMovies] = useState(
+    results.filter((result) => result.charAt(0) === 't')
+  );
+  const [actors, setActors] = useState(
+    results.filter((result) => result.charAt(0) === 'n')
+  );
 
-    PERSON {
-        id: string,
-        primaryName: string,
-        birthYear: number | null,
-        deathYear: number | null,
-        primaryProfession: [string],
-        knownForTitles: [string]
-    }
+  const loadMore = (type) => {
+    type === 'm' ? setMVisible(mVisible + 4) : setAVisible(aVisible + 4);
+  };
 
-*/
-
-  //kutsu näin: <ul>{mapPlaceholder()}</ul>
-  const mapMovieResults = () =>
-    results.map((mov) => (
-      <li key={mov}>
-        <p className='resultItem' onClick={(e) => onItemClick(mov, e)}>
-          {mov}
+  //kutsu näin: <ul>{mapActorResults()}</ul>
+  const mapResults = (data, visible) =>
+    data.slice(0, visible).map((res) => (
+      <li key={res}>
+        <p className='resultItem link' onClick={(e) => onItemClick(res, e)}>
+          {res}
         </p>
       </li>
     ));
 
-  /*  const mapPersonResults = () =>
-    results.map((name) => (
-      <li key={name}>
-        name
-        <button onClick={onClick(person, false)}>{person.primaryName}</button>
-      </li>
-    )); */
+  const resultDiv = (type, data, visibility) => (
+    <div>
+      <h2>{type}</h2>
+      <ul>{mapResults(data, visibility)}</ul>
+      {data && data.length > visibility ? (
+        <p
+          className='resultItem link'
+          onClick={() => (type === 'Movies' ? loadMore('m') : loadMore('n'))}
+        >
+          More results
+        </p>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 
   return (
     <div className='results'>
-      <h3>Movies</h3>
-      <ul>{mapMovieResults()}</ul>
+      {/* <div>
+        <h2>Movies</h2>
+        <ul>{mapMovieResults()}</ul>
+        {movies && movies.length > 4 ? (
+          <p className='resultItem link' onClick={() => loadMore('m')}>
+            More results
+          </p>
+        ) : (
+          <></>
+        )}
+      </div> */}
+      {resultDiv('Movies', movies, mVisible)}
+      {resultDiv('Actors', actors, aVisible)}
     </div>
   );
 };
