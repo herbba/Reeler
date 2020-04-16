c96a0da1bf7b7b1cee1aa0ce228d59adab3d487d
Ella Mäkelä <ella@makela.co>
2020-04-16 21:57:05 +0300

    react routes

---
+++ build/precache-manifest.f262de3dbeca741836e1fc8e1dc7e16b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee22e0e2d6c9d442c15bd5804db6529b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c56db0c3cd4e8a7706",
+    "url": "/static/css/main.c985b17f.chunk.css"
+  },
+  {
+    "revision": "7fd77e00975c75685003",
+    "url": "/static/js/2.6de51a18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6de51a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c56db0c3cd4e8a7706",
+    "url": "/static/js/main.48944491.chunk.js"
+  },
+  {
+    "revision": "11ebede490cbd7064573",
+    "url": "/static/js/runtime-main.87af969e.js"
+  },
+  {
+    "revision": "de50e2e9ca0fc52b57dbe9600b542e19",
+    "url": "/static/media/loader.488387b4.gif"
+  },
+  {
+    "revision": "e8c198675dd70548bfcaad21d58b8e33",
+    "url": "/static/media/logo.b21dc2dc.png"
+  }
+]);
