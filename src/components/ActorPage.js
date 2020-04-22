b4e84c298eb87ff06ee5629a742b62aceac66b04
Ella Mäkelä <ella@makela.co>
2020-04-22 20:24:41 +0300

    Michael Keatonin sivu UI:n mukainen

--- src/components/ActorPage.js
+++ src/components/ActorPage.js
@@ -10,6 +10,22 @@ const ActorPage = (props) => {
   const [titles, setTitles] = useState([]);
   const [movieIds, setMovieIds] = useState([]);
 
+  const mk = {
+    birthyear: props.location.state.birthyear,
+    birthday: 'September 5',
+    birthplace: 'Corapolis, Pennsylvania, USA',
+    deathyear: props.location.state.deathyear,
+    knownfortitles: props.location.state.knownfortitles,
+    nconst: props.location.state.nconst,
+    primaryname: props.location.state.primaryname,
+    birthname: 'Michael John Douglas',
+    primaryprofession: props.location.state.primaryprofession,
+    abstract:
+      "Quirky, inventive and handsome American actor Michael Keaton first achieved major fame with his door-busting performance as fast-talking ideas man Bill Blazejowski, alongside a nerdish morgue attendant ([[name:Henry Winkler]]), in Night [[title:Shift]] (1982). He played further comedic roles in [[title:Varokaa, isä on irti! (1983)]], [[title:Johnny - gangsterikuningas (1984)]], and [[movie:Beetlejuice (1988)]], earned further acclaim for his dramatic portrayal of Bruce Wayne / Batman in [[name:Tim Burton]]'s [[title:Batman (1989)]] and [[title:Batman - paluu (1992)]], and since then, has moved easily between film genres, ranging from drama and romantic comedy to thriller and action.",
+    fullbio:
+      'Keaton was born Michael John Douglas on September 5, 1951 in Coraopolis, Pennsylvania, to Leona Elizabeth (Loftus), a homemaker, and George A. Douglas, a civil engineer and surveyor. He is of Irish, as well as English, Scottish, and German, descent.',
+    images: ['mk1', 'mk2', 'mk3'],
+  };
   /* const knownForIds = props.location.state.results
     ? props.location.state.results.filter((item) => item.includes('tt'))
     : {}; */
@@ -18,7 +34,9 @@ const ActorPage = (props) => {
    * gets the data for knownfor titles
    */
   useEffect(() => {
-    setActor(props.location.state);
+    props.location.state.nconst === 'nm0000474'
+      ? setActor(mk)
+      : setActor(props.location.state);
     getTitles();
   }, [props.location.state]);
 
@@ -37,10 +55,10 @@ const ActorPage = (props) => {
   const mapFilmo = () => {
     return titles
       ? titles.map((t) => (
-          <li className='filmography' key={t.tconst}>
+          <li className='filmographyListItem' key={t.tconst}>
             {/* If link clicked, switch routes */}
             <Link
-              className='filmography'
+              className='link filmolink'
               to={{
                 pathname: `/titles/${t.tconst}`,
                 state: {
@@ -58,6 +76,8 @@ const ActorPage = (props) => {
             >
               {t.primarytitle}
             </Link>
+            <p className='filmorole'> - Role </p>
+            <p className='filmoyear'> {t.startyear}</p>
           </li>
         ))
       : '';
@@ -70,23 +90,71 @@ const ActorPage = (props) => {
       : '';
   };
 
-  return (
-    <div className='movieContainer'>
+  const mapImages = () => {
+    return actor.images ? (
+      actor.images.map((img) => (
+        <div className={`pageImagesColumn ${img}`}>
+          <img
+            className='pageImg'
+            src={require(`../images/${img}.jpg`)}
+            alt={img}
+          ></img>
+        </div>
+      ))
+    ) : (
+      <div>images not found</div>
+    );
+  };
+
+  return props.location.state.nconst === 'nm0000474' ? (
+    <div className='pageContainer'>
       {/*On click goes back to the previous page*/}
-      <p className='resultItem link' onClick={() => history.goBack()}>
+      {/* <p className='resultItem link' onClick={() => history.goBack()}>
         Back
       </p>
-
-      <div className='movieHeader'>
-        <h1 className='paddedText'>{actor.primaryname}</h1>
-        <p className='paddedText'>{styleProfession()}</p>
-        <div className='paddedText'>
+ */}
+      <div className='pageHeaderContainer'>
+        <h1 className='pageHeader'>{actor.primaryname}</h1>
+        <div className='pageImagesRow'>{mapImages()}</div>
+        <p className='pageAbstract'>{actor.abstract}</p>
+        <div className='pageAbstract'>
+          <p>
+            Born: {actor.birthday}
+            {', '}
+            {actor.birthyear ? actor.birthyear : 'unknown'}
+            {' in '}
+            {actor.birthplace}
+          </p>
+          <p>{actor.deathyear ? 'Died: ' + actor.deathyear : ''}</p>
+          <p>Birth Name: {actor.birthname}</p>
+        </div>
+      </div>
+      <div>
+        <h2 className='pageSubHeader'>Filmography</h2>
+        <ul className='filmography pageAbstract'>{mapFilmo()}</ul>
+      </div>
+      <div>
+        <h2 className='pageSubHeader'>Full bio</h2>
+        <p className='pageAbstract'>{actor.fullbio}</p>
+      </div>
+    </div>
+  ) : (
+    <div className='pageContainer'>
+      {/*On click goes back to the previous page*/}
+      {/* <p className='resultItem link' onClick={() => history.goBack()}>
+        Back
+      </p>
+ */}
+      <div className='pageHeaderContainer'>
+        <h1 className='pageHeader'>{actor.primaryname}</h1>
+        <p className='pageAbstract'>{styleProfession()}</p>
+        <div className='pageAbstract'>
           <p>Born: {actor.birthyear ? actor.birthyear : 'unknown'}</p>
           <p>{actor.deathyear ? 'Died: ' + actor.deathyear : ''}</p>
         </div>
       </div>
       <div>
-        <h2 className='paddedText'>Filmography</h2>
+        <h2 className='pageSubHeader'>Filmography</h2>
         <ul>{mapFilmo()}</ul>
       </div>
     </div>
